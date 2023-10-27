c7a4801e92f4996167d03153da50ecd563a00d95
mertsincan <sincan.mert@gmail.com>
2023-10-27 15:37:59 +0100

    Update for primecss

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default defineNuxtConfig({
         alias: aliasConfig.resolve.alias
     },
     primevue: {
+        usePrimeVue: false,
         options: {
             ripple: true
         },
