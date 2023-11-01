d424fe0dabf0f82c047de0e4b3768b54b375c060
Furkan Sezis <sezisfurkan24@gmail.com>
2023-11-01 13:11:06 +0300

    refactor

--- pages/templates/apollo.vue
+++ pages/templates/apollo.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-column justify-content-center align-items-between relative overflow-hidden">
-        <img src="../templates/assets/Hero.jpg" style="max-width: 1310px" class="overflow-hidden" />
-        <div class="absolute w-full" style="max-width: 1310px">
-            <div class="flex justify-content-between align-items-center" style="max-width: 1310px; padding-left: 7rem">
+    <div class="flex flex-column justify-content-center relative overflow-hidden">
+        <img src="../templates/assets/Hero.jpg" class="overflow-hidden w-full" />
+        <div class="absolute w-full pl-8">
+            <div class="flex justify-content-between align-items-center" style="max-width: 1310px">
                 <div
                     class="flex flex-column justify-content-center align-items-start gap-4 border-round-3xl"
                     style="
@@ -14,9 +14,9 @@
                         max-height: 265px;
                     "
                 >
-                    <div class="flex flex-column justify-content-center align-items-start" style="padding: 36px 28px; gap: 32px">
+                    <div class="flex flex-column justify-content-center align-items-start gap-5" style="padding: 36px 28px">
                         <img src="../templates/assets/Logo.svg" alt="1" />
-                        <span style="color: #ffffff">Vue template with mode options, menu layouts, sample apps, pre-made pages, and 24 PrimeVue themes.</span>
+                        <span class="text-white">Vue template with mode options, menu layouts, sample apps, pre-made pages, and 24 PrimeVue themes.</span>
                         <div class="flex flex-column justify-content-center align-items-start gap-3 align-self-stretch">
                             <div class="flex align-items-center gap-3 align-self-stretch">
                                 <Button label="Live Demo" class="flex justify-content-center align-items-center flex-1 surface-900 border-900" style="padding: 10.5px 17.5px; gap: 7px; border-radius: 48px"></Button>
@@ -25,23 +25,23 @@
                         </div>
                         <div class="flex align-items-center gap-3">
                             <div class="flex justify-content-center align-items-center gap-2">
-                                <i class="pi pi-github" style="color: #ffffff" />
-                                <a class="flex justify-content-center" style="color: #ffffff">Get Support</a>
+                                <i class="pi pi-github text-white" />
+                                <a class="flex justify-content-center text-white">Get Support</a>
                             </div>
                             <div class="flex justify-content-center align-items-center gap-2">
-                                <i class="pi pi-book" style="color: #ffffff" />
-                                <a class="flex justify-content-center" style="color: #ffffff">Read Docs</a>
+                                <i class="pi pi-book text-white" />
+                                <a class="flex justify-content-center text-white">Read Docs</a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="" style="height: 700px; width: 700px">
+                <div style="height: 700px; width: 700px">
                     <img src="../templates/assets/Group_4.svg" alt="1" class="w-full h-full pt-7" />
                 </div>
             </div>
         </div>
     </div>
-    <div class="flex flex-column align-items-center gap-6" style="padding: 80px 120px; width: 1310px">
+    <div class="flex flex-column align-items-center gap-6" style="padding: 80px 120px">
         <div class="flex flex-column align-items-center" style="gap: 20px; width: 710px">
             <span class="flex flex-column align-self-stretch text-900 text-center text-6xl font-semibold" style="line-height: 56px; width: 710px"> <span>Integration with </span>Existing Vite Applications </span>
             <span class="flex text-center text-base font-normal" style="line-height: 21px; width: 710px"
@@ -50,18 +50,9 @@
         </div>
         <div class="border-round-2xl" style="max-width: 930px; max-height: 596.25px; background-color: #e0e0f7; backdrop-filter: blur(2px)">
             <div class="video relative">
-                <img src="../templates/assets/Dashboard.svg" class="flex align-items-start" style="width: 100%; height: 100%; object-fit: cover; filter: blur(2px)" alt="" />
-                <Button
-                    icon="pi pi-play text-4xl ml-2"
-                    class="absolute top-50 right-50"
-                    rounded
-                    :pt="{
-                        icon: {
-                            class: 'bg-red-500'
-                        }
-                    }"
-                    style="margin-right: -3rem; margin-top: -3rem; height: 5rem; width: 5rem; background-color: #fff"
-                ></Button>
+                <img src="../templates/assets/Dashboard.svg" class="flex align-items-start w-full h-full" style="object-fit: cover; filter: blur(2px)" alt="" />
+
+                <img src="../templates/assets/PLay.svg" class="absolute top-50 right-50 cursor-pointer w-full h-full" style="margin-right: -5rem; margin-top: -5rem; height: 10rem; width: 10rem" alt="" />
             </div>
         </div>
     </div>
